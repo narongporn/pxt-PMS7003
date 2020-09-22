ed71d35d5b1e2038dc7f30fb0cbd401ce67e3643
narongporn <43835385+narongporn@users.noreply.github.com>
2020-09-22 17:47:05 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-serial.onDataReceived("BM", function () {
-    led.toggle(2, 2)
-    PMS7003.ReadData()
-    if (PMS7003.CalcChecksum()) {
-        basic.showNumber(PMS7003.DataPM1())
-    } else {
-    	
-    }
-})
-PMS7003.ConnectSensor(SerialPin.P1, SerialPin.P2)
-basic.forever(function () {
-	
-})
+
